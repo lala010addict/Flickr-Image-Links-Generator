0ba6632bfffd32cbbdf9f6663ace8423463a020d
jieyingpeng <jipeng@syr.edu>
2016-01-17 22:53:22 -0800

    first commit

---
+++ controller.js
@@ -0,0 +1,38 @@
+myFlickr.controller('menu', ['$scope', "$http", 'PhotoSet', "$location", '$routeParams', function($scope, $http, PhotoSet, $location, $routeParams) {
+  $scope.data = {}
+
+
+
+  $scope.photos = {};
+
+
+  $scope.getPhotos = function(item) {
+    //  console.log(item);
+    PhotoSet.getPhotos(item)
+      .success(function(data) {
+
+        $scope.photos = data.photoset.photo
+    
+      })
+  }
+
+  //booh!
+
+  // $scope.getUser = function(username) {
+  //   $scope.pix = [];
+  //   $scope.idHolder = []
+  //   console.log(username)
+  //   PhotoSet.getUser(username)
+
+  //   .success(function(data) {
+  //     console.log(data)
+  //     if (data.code === 1 || undefined) {
+  //       alert(data.message)
+  //     } else {
+  //       $scope.getSet(data.user.id)
+  //     }
+  //   })
+  // }
+
+
+}])
